d8f65fd96c67e7859873c4dc846fc52e8b5521db
Учитель <teacher@TOP.ACADEMY>
2024-10-10 10:16:09 +0300

    section reservation - left block

---
+++ post.php
@@ -0,0 +1,6 @@
+
+
+
+Привет <?php echo htmlspecialchars($_POST['name']); ?>
+Твой телефон <?php echo htmlspecialchars($_POST['phone']); ?>
+Слушаешь радио волну <?php echo htmlspecialchars($_POST['vawe']); ?>
